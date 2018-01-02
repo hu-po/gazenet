346c2cee55513eb9babfeb4998d8005f0962207c
ook-yoga <hugo.p.cmu@gmail.com>
2018-01-02 09:22:42 +0800

    test dataset collector

--- data/data_collector.py
+++ data/data_collector.py
@@ -1,38 +1,51 @@
 import cv2
+import random
+import time
 import os
 import sys
+
 mod_path = os.path.abspath(os.path.join('..'))
 sys.path.append(mod_path)
 from src.config import DATA_DIR
-
 
 '''
 This python file collects gaze data using the webcamera.
 '''
 
 # Dataset parameters
-DATASET_NAME = 'test'
-NUM_IMAGES = 10
+DATASET_NAME = '020118_fingers'
+NUM_IMAGES = 50
 
 # Create local data dir
 dataset_dir = os.path.join(DATA_DIR, DATASET_NAME)
 if not os.path.exists(dataset_dir):
     os.mkdir(dataset_dir)
 
-# Use cv2 to save and
-for _ in range(NUM_IMAGES):
+# Possible gaze_locations
+gaze_locations = [1, 2, 3, 4]
 
+for i in range(NUM_IMAGES):
     # Plot target gaze location on screen
+    label = random.choice(gaze_locations)
+    print('Upcoming gaze location: ', label)
+
+    # Set up video capture device on webcam
+    webcam = cv2.VideoCapture(0)
 
     # Countdown on target gaze location
+    time.sleep(3)
 
     # Snap image from webcamera
-    video_capture = cv2.VideoCapture(0)
+    snap = webcam.grab()
+    ret, frame = webcam.retrieve()
 
-    # Extract frame
-    ret, frame = video_capture.read()
+    # # Show image
+    # cv2.imshow('Video', frame)
+    # cv2.waitKey()
 
-    # Show image
-    cv2.imshow('Video', frame)
+    # Save image
+    filename = '%s_%s.jpg' % (i, label)
+    cv2.imwrite(os.path.join(dataset_dir, filename), frame)
 
-    cv2.waitKey()
+    # Delete camera object to reset buffer
+    del webcam
