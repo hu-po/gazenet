2f068c13c50be86bdd3449cfb41387784f52fe72
ook-yoga <hugo.p.cmu@gmail.com>
2018-01-04 20:16:39 +0800

    Fixed EndOfSequence errors, data pipeline is working

--- src/gaze_model.py
+++ src/gaze_model.py
@@ -1,11 +1,5 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
-import os, sys
-
-mod_path = os.path.abspath(os.path.join('..'))
-sys.path.append(mod_path)
-
-from src.helper_func import define_scope
 
 '''
 
@@ -27,34 +21,35 @@ class GazeModel(object):
         self.image = image
         self.label = label
         self.config = config
-        self.predict
-        self.optimize
-        self.accuracy
+        self.predict = self.predict_func()
+        self.optimize = self.optimize_func()
+        self.accuracy = self.accuracy_func()
 
-    @define_scope(initializer=slim.xavier_initializer())
-    def predict(self):
-        x = self.image
-        # tf.summary.image('input_image', x)
-        x = slim.conv2d(x, 32, [3, 3], scope='conv1')
-        x = slim.conv2d(x, 64, [3, 3], scope='conv2')
-        x = slim.max_pool2d(x, [2, 2], scope='pool1')
-        x = slim.flatten(x)
-        x = slim.fully_connected(x, 128)
-        x = slim.fully_connected(x, 64)
-        x = slim.fully_connected(x, self.config['output_classes'], tf.nn.softmax)
+    def predict_func(self):
+        with tf.variable_scope('predict', initializer=slim.xavier_initializer(), reuse=tf.AUTO_REUSE):
+            x = self.image
+            tf.summary.image('input_image', x)
+            x = slim.conv2d(x, 32, [3, 3], scope='conv1')
+            x = slim.conv2d(x, 64, [3, 3], scope='conv2')
+            x = slim.conv2d(x, 64, [3, 3], scope='conv3')
+            x = slim.max_pool2d(x, [2, 2], scope='pool1')
+            x = slim.flatten(x)
+            x = slim.fully_connected(x, 128)
+            x = slim.fully_connected(x, 64)
+            x = slim.fully_connected(x, self.config['output_classes'], tf.nn.softmax)
         return x
 
-    @define_scope
-    def optimize(self):
-        loss = tf.losses.sparse_softmax_cross_entropy(labels=self.label, logits=self.predict)
-        tf.summary.scalar('loss', loss)
-        optimizer = tf.train.RMSPropOptimizer(self.config['learning_rate'])
+    def optimize_func(self):
+        with tf.variable_scope('optimize', reuse=tf.AUTO_REUSE):
+            loss = tf.losses.sparse_softmax_cross_entropy(labels=self.label, logits=self.predict)
+            tf.summary.scalar('loss', loss)
+            optimizer = tf.train.RMSPropOptimizer(self.config['learning_rate'])
         return optimizer.minimize(loss)
 
-    @define_scope
-    def accuracy(self):
-        predicted_label = tf.cast(tf.argmax(self.predict, 1), tf.int32)
-        mistakes = tf.not_equal(self.label, predicted_label)
-        accuracy = tf.reduce_mean(tf.cast(mistakes, tf.float32))
-        tf.summary.scalar('accuracy', accuracy)
+    def accuracy_func(self):
+        with tf.variable_scope('accuracy', reuse=tf.AUTO_REUSE):
+            predicted_label = tf.cast(tf.argmax(self.predict, 1), tf.int32)
+            mistakes = tf.not_equal(self.label, predicted_label)
+            accuracy = tf.reduce_mean(tf.cast(mistakes, tf.float32))
+            tf.summary.scalar('accuracy', accuracy)
         return accuracy
