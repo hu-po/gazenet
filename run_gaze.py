8a40b2c4f5c7b829cfda72bd9a3eb47275f0d150
ook-yoga <hugo.p.cmu@gmail.com>
2018-01-22 19:46:13 +0800

    Freezing graph using tensorflow utilities

--- run_gaze.py
+++ run_gaze.py
@@ -13,15 +13,27 @@ from src.utils.cam_utils import WebcamVideoStream, FPS
 
 '''
 This file is used to run the Gaze Net. It takes images from your webcam, feeds them through the model
-and outputs your current gaze location on the screen. Make sure to fullscreen it!
+and outputs your current gaze location on the screen.
 
 Sources:
 [1] https://github.com/datitran/object_detector_app
 [2] https://github.com/tensorflow/serving/issues/488
 '''
 
-CHKPT = '/home/ook/repos/gazeGAN/local/models/gaze_resnet_rb_kernel_3_fc_layers_64_batch_norm_False_dimred_stride_4_rb_feat_8_num_rb_2_dimred_feat_32_dimred_kernel_4_learning_rate_0.01/checkpoint'
+CHKPT = 'frozen_graph.pb'
 
+'''
+
+To freeze the graph use the following:
+
+python freeze_graph.py \
+--input_graph=graph.pbtxt \
+--input_checkpoint=model.ckpt-0 \
+--output_graph=frozen_graph.pb \
+--output_node_names=gaze_resnet/output/BiasAdd \
+--variable_names_blacklist=IsVariableInitialized,globalstep
+
+'''
 
 SESS_DICT = {}
 def get_session(model_id):
@@ -41,14 +53,14 @@ def gaze_inference(image_np, sess, model_graph):
     image_np_expanded = np.expand_dims(image_np, axis=0)
 
     # Get input and output to the gaze model
-    image_input = model_graph.get_tensor_by_name('input_image')
+    input_image = model_graph.get_tensor_by_name('input_image')
     predict = model_graph.get_tensor_by_name('predict')
 
     # Actual detection.
-    gaze_output = sess.run(predict, feed_dict={image_input: image_np_expanded})
+    gaze_output = sess.run(predict, feed_dict={input_image: image_np_expanded})
 
     # Visualization of the results of a detection.
-    canvas = cam_utils.screen_plot(gaze_output, image=image_input, window_name=conf.window_name)
+    canvas = cam_utils.screen_plot(gaze_output, image=input_image, window_name=conf.window_name)
     return canvas
 
 
